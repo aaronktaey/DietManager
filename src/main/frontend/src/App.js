318bb51dcde61430cf54ffc1ff24039b3e9479e0
aaronktaey <aaronktaey@gmail.com>
2023-02-23 10:04:09 +0900

    라우터 추가 , 홈 / 404 페이지 추가

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,11 +1,29 @@
 
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import NotFound from './NotFound';
+import Home from './Home';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
+  //  const [hello, setHello] = useState('')
+
+  //   useEffect(() => {
+  //       axios.get('/api/hello')
+  //       .then(response => setHello(response.data))
+  //       .catch(error => console.log('아직 API가 없어...'))
+  //   }, []);
+
+    return (
+      <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/home" element={<Home/>}></Route>
+          <Route path="*" element={<NotFound/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
-  );
+    );
 }
 
 export default App;
---
+++ src/main/frontend/src/Home.js
@@ -0,0 +1,15 @@
+import "./Home.css"
+
+function Home() {
+  return (
+    <div id="home" className="home-container">
+      <div className="home-item">
+        <h1>
+          Page Home.
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
