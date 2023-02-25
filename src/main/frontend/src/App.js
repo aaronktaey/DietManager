de7c6b419c4fe50eef0c5fc38e9ef2b2b5ff5b71
yeong-ho <dkf4928@naver.com>
2023-02-25 18:50:20 +0900

    스프링 리액트 연동 테스트

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,42 +1,20 @@
-
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
-import {BrowserRouter, Routes, Route} from "react-router-dom"
-import NotFound from './NotFound';
-import Home from './Home';
-import Login from './Login';
-import Nav from './Devnav';
-import Profile from './Profile';
-import Calculate from './Calculate';
-import Detail from './Detail';
-import Advice from './Advice';
 
 function App() {
-  //  const [hello, setHello] = useState('')
+  const [hello, setHello] = useState('')
 
-  //   useEffect(() => {
-  //       axios.get('/api/hello')
-  //       .then(response => setHello(response.data))
-  //       .catch(error => console.log('아직 API가 없어...'))
-  //   }, []);
+  useEffect(() => {
+    axios.get('/api/hello')
+        .then(response => setHello(response.data))
+        .catch(error => console.log(error))
+  }, []);
 
-    return (
-      <div className="App">
-      <BrowserRouter>
-        <Nav/>
-        <Routes>
-          <Route path="/" element={<Login/>}></Route>
-          <Route path="/home" element={<Home/>}></Route>
-          <Route path="/login" element={<Login/>}></Route>
-          <Route path="/profile" element={<Profile/>}></Route>
-          <Route path="/calculate" element={<Calculate/>}></Route>
-          <Route path="/detail" element={<Detail/>}></Route>
-          <Route path="/advice" element={<Advice/>}></Route>
-          <Route path="*" element={<NotFound/>}></Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
-    );
+  return (
+      <div>
+        백엔드에서 가져온 데이터입니다 : {hello}
+      </div>
+  );
 }
 
 export default App;
