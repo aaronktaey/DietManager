7c10cea976ba5ab853b151a8cf6b030fb4f5b293
yeong-ho <dkf4928@naver.com>
2023-03-01 17:13:57 +0900

    ì컨트롤러 주석 처리

--- src/main/java/project/dietmanager/controller/HomeController.java
+++ src/main/java/project/dietmanager/controller/HomeController.java
@@ -5,11 +5,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+//@Controller
 @Slf4j
 public class HomeController {
 
-    @GetMapping("/")
+//    @GetMapping("/")
     public String home() {
         log.info("Controller.home!");
         return "";
