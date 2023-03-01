2dbbb23512b7be85ba74d69b20bdf41e18ef333f
yeong-ho <dkf4928@naver.com>
2023-03-01 16:27:59 +0900

    ë¦ë¦리액트 빌드

--- src/main/java/project/dietmanager/controller/HomeController.java
+++ src/main/java/project/dietmanager/controller/HomeController.java
@@ -1,14 +1,17 @@
 package project.dietmanager.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+@Slf4j
 public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "home";
+        log.info("Controller.home!");
+        return "";
     }
 }
