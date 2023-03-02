3f6f70a20d3930def63a0cde9626a0a04d1eb7f3
yeong-ho <dkf4928@naver.com>
2023-03-02 22:00:20 +0900

    build test

--- src/test/java/project/dietmanager/DietmanagerApplicationTests.java
+++ src/test/java/project/dietmanager/DietmanagerApplicationTests.java
@@ -3,10 +3,10 @@ package project.dietmanager;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class DietmanagerApplicationTests {
 
-//	@Test
+	@Test
 	void contextLoads() {
 	}
 
