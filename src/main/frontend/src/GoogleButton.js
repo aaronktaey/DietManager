aaf7dd7ae817202cfa3e50ba6307a3285d762618
aaronktaeyHome <aaronktaey@gmail.com>
2023-03-22 22:02:17 +0900

    구글로그인 POST 파라미터 제거 URL에 주소 포트 추가

--- src/main/frontend/src/GoogleButton.js
+++ src/main/frontend/src/GoogleButton.js
@@ -1,47 +1,46 @@
 // import "./Login.css"
-import GoogleLogin from 'react-google-login';
-import {useEffect, useCallback} from 'react';
-import { gapi } from 'gapi-script';
-import axios from 'axios';
+import GoogleLogin from "react-google-login";
+import { useEffect, useCallback } from "react";
+import { gapi } from "gapi-script";
+import axios from "axios";
 
-const clientId = '54088883439-vjqo4c1u6727cdtj175sf8kbbsvuup7l.apps.googleusercontent.com';
+const clientId =
+  "54088883439-vjqo4c1u6727cdtj175sf8kbbsvuup7l.apps.googleusercontent.com";
 
 const GoogleButton = () => {
-    useEffect(()=>{
-        function start(){
-            gapi.client.init({
-                clientId,
-                scope: 'email',
-            });
-        }
-
-        gapi.load('client:auth2',start);
-
-    },[]);
-
-    const onSuccess = (res) =>{
-        console.log(res);
-        axios.post('api/login/google', res)
-        .then(response => alert(response))
-        .catch(error => alert('구글 로그인 실패.',error))
+  useEffect(() => {
+    function start() {
+      gapi.client.init({
+        clientId,
+        scope: "email",
+      });
     }
 
-    const onFailure = (res) =>{
-        alert('로그인 실패...',res.error);
-    }
+    gapi.load("client:auth2", start);
+  }, []);
 
+  const onSuccess = (res) => {
+    console.log(res);
+    axios
+      .post("http://localhost:8080/api/login/google")
+      .then((response) => alert(response))
+      .catch((error) => alert("구글 로그인 실패.", error));
+  };
 
+  const onFailure = (res) => {
+    alert("로그인 실패...", res.error);
+  };
 
-    return (
-        <div>
-            <GoogleLogin
-                clientId={clientId}
-                buttonText="구글 아이디로 로그인"
-                onSuccess={onSuccess}
-                onFailure={onFailure}
-            />
-        </div>
-    );
-}
+  return (
+    <div>
+      <GoogleLogin
+        clientId={clientId}
+        buttonText="구글 아이디로 로그인"
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+      />
+    </div>
+  );
+};
 
 export default GoogleButton;
