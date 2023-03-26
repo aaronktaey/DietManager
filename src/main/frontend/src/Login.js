cc5c3830fd04acf9f6483d4694ab1aadd4c63bab
aaronktaeyHome <aaronktaey@gmail.com>
2023-03-26 19:28:29 +0900

    프로필화면 추가

--- src/main/frontend/src/Login.js
+++ src/main/frontend/src/Login.js
@@ -1,84 +1,92 @@
-import "./Login.css"
-import axios from 'axios';
-import {useEffect, useState} from 'react';
-import GoogleButton from './GoogleButton';
+import "./Login.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import GoogleButton from "./GoogleButton";
 
 function Login() {
-    const [toggle, setToggle] = useState(true);
-    const [loginId, setloginId] = useState('');
-    const [password, setpassword] = useState('');
+  const [toggle, setToggle] = useState(true);
+  const [loginId, setloginId] = useState("");
+  const [password, setpassword] = useState("");
 
-    const handleloginId = (e) =>{
-        setloginId(e.target.value);
-        console.log(e.target.value)
+  const handleloginId = (e) => {
+    setloginId(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const handlepassword = (e) => {
+    setpassword(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const handleToggle = (e) => {
+    setToggle(toggle ? false : true);
+  };
+
+  const handleLoginBtnClick = (e) => {
+    if (toggle) {
+      axios
+        .post("/api/login", {
+          loginId: loginId,
+          password: password,
+        })
+        .then((response) => alert(response))
+        .catch((error) => alert("로그인 실패.", error));
+    } else {
+      axios
+        .post("/api/signup", {
+          loginId: loginId,
+          password: password,
+        })
+        .then((response) => alert(response))
+        .catch((error) => alert("회원가입 실패.", error));
     }
+  };
 
-    const handlepassword = (e) =>{
-        setpassword(e.target.value);
-        console.log(e.target.value)
-    }
+  // 페이지 렌더링 후 가장 처음 호출되는 함수
+  useEffect(
+    () => {
+      console.log("컴포넌트 렌더링 끝");
+    },
+    // 페이지 호출 후 처음 한번만 호출될 수 있도록 [] 추가
+    []
+  );
 
-    const handleToggle = (e) =>{
-        setToggle(toggle ? false : true);
-    }
-
-    const handleLoginBtnClick = (e) =>{
-        if(toggle){
-            axios.post('/api/login',
-                {
-                    loginId : loginId,
-                    password: password,
-                }
-            )
-                .then(response => alert(response))
-                .catch(error => alert('로그인 실패.',error))
-        }else{
-            axios.post('/api/signup',
-                {
-                    loginId : loginId,
-                    password: password,
-                }
-            )
-                .then(response => alert(response))
-                .catch(error => alert('회원가입 실패.',error))
-        }
-    }
-
-    // 페이지 렌더링 후 가장 처음 호출되는 함수
-    useEffect(() => {
-            console.log('컴포넌트 렌더링 끝');
-        },
-        // 페이지 호출 후 처음 한번만 호출될 수 있도록 [] 추가
-        [])
-
-    return (
-        <div id="login" className="login-container">
-            <div className="login-item">
-                <h1>
-                    {toggle ? '로그인' : '회원가입'}
-                </h1>
-            </div>
-            <div className="login-item">
-                <button type="button" onClick={handleToggle}>{toggle ? '회원가입 하기' : '로그인 하기'}</button>
-            </div>
-            <div className="login-item">
-                <label htmlFor="loginId">아 이 디 : </label>
-                <input type="text" name="loginId" value={loginId} onChange={handleloginId}/>
-            </div>
-            <div className="login-item">
-                <label htmlFor="password">비밀번호 : </label>
-                <input type="password" name="password" value={password} onChange={handlepassword}/>
-            </div>
-            <div className="login-item">
-                <button type="button" onClick={handleLoginBtnClick}>{toggle ? '로그인' : '회원가입'}</button>
-            </div>
-            <div className="login-item">
-                {toggle ? <GoogleButton/> : false}
-            </div>
-
-
-        </div>
-    );
+  return (
+    <div id="login" className="login-container">
+      <div className="login-item">
+        <h1>{toggle ? "로그인" : "회원가입"}</h1>
+      </div>
+      <div className="login-item">
+        <button type="button" onClick={handleToggle}>
+          {toggle ? "회원가입 하기" : "로그인 하기"}
+        </button>
+      </div>
+      <div className="login-item">
+        <label htmlFor="loginId">아 이 디 : </label>
+        <input
+          type="text"
+          name="loginId"
+          value={loginId}
+          onChange={handleloginId}
+        />
+      </div>
+      <div className="login-item">
+        <label htmlFor="password">비밀번호 : </label>
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={handlepassword}
+        />
+      </div>
+      <div className="login-item">
+        <button type="button" onClick={handleLoginBtnClick}>
+          {toggle ? "로그인" : "회원가입"}
+        </button>
+      </div>
+      <div className="login-item">{toggle ? <GoogleButton /> : false}</div>
+    </div>
+  );
 }
 
 export default Login;
--- src/main/frontend/src/Profile.js
+++ src/main/frontend/src/Profile.js
@@ -11,31 +11,116 @@ const Profile = () => {
       "https://photo.newsen.com/news_photo/2022/08/19/202208190914415510_1.jpg",
     Email: "minji@win.co.kr",
   };
+  const [isEdit, setIsEdit] = useState(false);
   const [name, setName] = useState(data.Name);
   const [weight, setWeight] = useState(data.Weight);
   const [height, setHeight] = useState(data.Height);
   const [photoUrl, setPhotoUrl] = useState(data.PhotoUrl);
-  const [emailAddress, setEmailAddress] = useState(data.Email);
+  const [email, setEmail] = useState(data.Email);
+
+  const handleEditToggle = () => {
+    setIsEdit(!isEdit);
+  };
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleWeightChange = (e) => {
+    setWeight(e.target.value);
+  };
+
+  const handleHeightChange = (e) => {
+    setHeight(e.target.value);
+  };
+
+  const handlePhotoUrlChange = (e) => {
+    setPhotoUrl(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
 
   return (
     <div className="profile-container">
       <div className="profile-item">
-        <h1>{name}</h1>
+        <button onClick={handleEditToggle}>{isEdit ? "Save" : "Edit"}</button>
       </div>
-      <div className="profile-item">
-        <img width={400} src={photoUrl} alt="Profile photo" />
-      </div>
-      <div className="profile-item">
-        <p>Weight: {weight}</p>
-      </div>
-      <div className="profile-item">
-        <p>Height: {height}</p>
-      </div>
-      <div className="profile-item">
-        <p>
-          Email: <a href={`mailto:${emailAddress}`}>{emailAddress}</a>
-        </p>
-      </div>
+      {isEdit ? (
+        <>
+          <div className="profile-item">
+            <label htmlFor="name-input">Name: </label>
+            <input
+              type="text"
+              id="name-input"
+              value={name}
+              onChange={handleNameChange}
+            />
+          </div>
+          <br />
+
+          <div className="profile-item">
+            <label htmlFor="weight-input">Weight: </label>
+            <input
+              type="text"
+              id="weight-input"
+              value={weight}
+              onChange={handleWeightChange}
+            />
+          </div>
+          <br />
+          <div className="profile-item">
+            <label htmlFor="height-input">Height: </label>
+            <input
+              type="text"
+              id="height-input"
+              value={height}
+              onChange={handleHeightChange}
+            />
+          </div>
+          <br />
+          <div className="profile-item">
+            <label htmlFor="photo-url-input">Photo URL: </label>
+            <input
+              type="text"
+              id="photo-url-input"
+              value={photoUrl}
+              onChange={handlePhotoUrlChange}
+            />
+          </div>
+          <br />
+          <div className="profile-item">
+            <label htmlFor="email-address-input">Email : </label>
+            <input
+              type="email"
+              id="email-address-input"
+              value={email}
+              onChange={handleEmailChange}
+            />
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="profile-item">
+            <h1>{name}</h1>
+          </div>
+          <div className="profile-item">
+            <p>Weight: {weight}</p>
+          </div>
+          <div className="profile-item">
+            <p>Height: {height}</p>
+          </div>
+          <div className="profile-item">
+            <img width={400} src={photoUrl} alt="Profile photo" />
+          </div>
+          <div className="profile-item">
+            <p>
+              Email: <a href={`mailto:${email}`}>{email}</a>
+            </p>
+          </div>
+        </>
+      )}
     </div>
   );
 };
