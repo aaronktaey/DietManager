02537e28c418c3e7bd47364adafaa93ca1716ec1
aaronktaeyHome <aaronktaey@gmail.com>
2023-03-26 18:24:44 +0900

    프로필화면

--- src/main/frontend/src/Home.js
+++ src/main/frontend/src/Home.js
@@ -1,24 +1,24 @@
-import "./Home.css"
-import {useEffect, useState} from 'react';
+import "./Home.css";
+import { useEffect, useState } from "react";
 
 function Home() {
-  const [ingredient, setIngredient] = useState('');
-  const [quantity, setQuantity] = useState('');
-  const [unit, setUnit] = useState('');
+  const [ingredient, setIngredient] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [unit, setUnit] = useState("");
 
   const handleIngredientSelect = (e) => {
     setIngredient(e.target.value);
-  }
+  };
   const handleInputQuantity = (e) => {
     setQuantity(e.target.value);
-  }
+  };
   const handleUnitSelect = (e) => {
     setUnit(e.target.value);
-  }
+  };
   const addItem = (e) => {
-    const home = document.querySelector('#home');
-    const newItem = document.createElement('div');
-    newItem.setAttribute('class','home-item');
+    const home = document.querySelector("#home");
+    const newItem = document.createElement("div");
+    newItem.setAttribute("class", "home-item");
     newItem.innerHTML = `
       <select id="ingredient" value="${ingredient}" onChange="handleIngredientSelect">
         <option value="banana">바나나</option>
@@ -33,12 +33,10 @@ function Home() {
       </select>
     `;
     home.append(newItem);
-    
-  }
+  };
 
-
-  (()=>{
-    console.log('rendering!');
+  (() => {
+    console.log("rendering!");
   })();
 
   return (
@@ -47,12 +45,21 @@ function Home() {
         <button onClick={addItem}>추가</button>
       </div>
       <div className="home-item">
-        <select id="ingredient" value={ingredient} onChange={handleIngredientSelect}>
+        <select
+          id="ingredient"
+          value={ingredient}
+          onChange={handleIngredientSelect}
+        >
           <option value="banana">바나나</option>
           <option value="apple">사과</option>
           <option value="orange">오렌지</option>
         </select>
-        <input id="quantity" value={quantity} onChange={handleInputQuantity} placeholder="수량"/>
+        <input
+          id="quantity"
+          value={quantity}
+          onChange={handleInputQuantity}
+          placeholder="수량"
+        />
         <select id="unit" value={unit} onChange={handleUnitSelect}>
           <option value="개">개</option>
           <option value="조각">조각</option>
--- src/main/frontend/src/Profile.js
+++ src/main/frontend/src/Profile.js
@@ -1,15 +1,43 @@
-import "./Profile.css"
+import { useState } from "react";
+import "./Profile.css";
 
-function Profile() {
-    return (
-        <div id="profile" className="profile-container">
-            <div className="profile-item">
-                <h1>
-                    Page Profile.
-                </h1>
-            </div>
-        </div>
-    );
-}
+const Profile = () => {
+  const data = {
+    // db에서 조회된 데이터
+    Name: "민지",
+    Weight: "43",
+    Height: "169",
+    PhotoUrl:
+      "https://photo.newsen.com/news_photo/2022/08/19/202208190914415510_1.jpg",
+    Email: "minji@win.co.kr",
+  };
+  const [name, setName] = useState(data.Name);
+  const [weight, setWeight] = useState(data.Weight);
+  const [height, setHeight] = useState(data.Height);
+  const [photoUrl, setPhotoUrl] = useState(data.PhotoUrl);
+  const [emailAddress, setEmailAddress] = useState(data.Email);
+
+  return (
+    <div className="profile-container">
+      <div className="profile-item">
+        <h1>{name}</h1>
+      </div>
+      <div className="profile-item">
+        <img width={400} src={photoUrl} alt="Profile photo" />
+      </div>
+      <div className="profile-item">
+        <p>Weight: {weight}</p>
+      </div>
+      <div className="profile-item">
+        <p>Height: {height}</p>
+      </div>
+      <div className="profile-item">
+        <p>
+          Email: <a href={`mailto:${emailAddress}`}>{emailAddress}</a>
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default Profile;
