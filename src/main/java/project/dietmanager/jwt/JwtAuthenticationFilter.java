d4d143ad58200708a91ef75c1f044bb8ca3fa1dd
yeonghoS <dkf4928@naver.com>
2023-03-31 13:22:22 +0900

    build test

--- src/main/java/project/dietmanager/jwt/JwtAuthenticationFilter.java
+++ src/main/java/project/dietmanager/jwt/JwtAuthenticationFilter.java
@@ -22,7 +22,7 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        System.out.println("build test!!!!!!!!!!!!!!!!!!!! ");
+        System.out.println("build test!!!!!!!!!!!!!!!!!!");
         String URI = ((HttpServletRequest) request).getRequestURI();
 
         if (!Arrays.stream(whiteList).anyMatch(a -> URI.contains(a)) && !URI.equals("/")) {
