39adbe21465ad835b443d13105f7bdae0cdb620e
yeonghoS <dkf4928@naver.com>
2023-03-31 14:13:57 +0900

    불필요한 코드 삭제

--- src/main/frontend/src/GoogleButton.js
+++ src/main/frontend/src/GoogleButton.js
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
 import { gapi } from "gapi-script";
-import axios from "axios";
 
 const clientId =
     "54088883439-vjqo4c1u6727cdtj175sf8kbbsvuup7l.apps.googleusercontent.com";
