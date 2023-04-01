6fc69cec867a10c569b4f07f24a54b45b27aaea7
yeong-ho <dkf4928@naver.com>
2023-04-01 18:20:31 +0900

    ì쉘 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-BUILD_JAR=$(ls /home/ubuntu/DietManager/build/libs/*.jar)
+BUILD_JAR=$(ls /home/ec2-user/action/build/libs/*.jar)
 JAR_NAME=$(basename $BUILD_JAR)
+echo "> build 파일명: $JAR_NAME" >> /home/ec2-user/action/deploy.log
 
-DEPLOY_PATH=/home/ubuntu/DietManager/
+echo "> build 파일 복사" >> /home/ec2-user/action/deploy.log
+DEPLOY_PATH=/home/ec2-user/action/
 cp $BUILD_JAR $DEPLOY_PATH
 
+echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ec2-user/action/deploy.log
 CURRENT_PID=$(pgrep -f $JAR_NAME)
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ec2-user/action/deploy.log
 else
   echo "> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
@@ -17,4 +20,5 @@ else
 fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-java -jar $DEPLOY_JAR
+echo "> DEPLOY_JAR 배포"    >> /home/ec2-user/action/deploy.log
+nohup java -jar $DEPLOY_JAR
