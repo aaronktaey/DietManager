283c3d8e6ea42f53175fafa2f370ac6b65405110
aaronktaeyHome <aaronktaey@gmail.com>
2023-04-05 23:39:29 +0900

    foods 데이터 조회

--- src/main/frontend/src/Additem.js
+++ src/main/frontend/src/Additem.js
@@ -1,9 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 function AddItem() {
   const [ingredient, setIngredient] = useState("");
   const [quantity, setQuantity] = useState("");
-  const [setUnit] = useState("");
-  
+  const [unit, setUnit] = useState("");
+
   const handleIngredientSelect = (e) => {
     setIngredient(e.target.value);
   };
@@ -24,7 +24,7 @@ function AddItem() {
         <option value="orange">오렌지</option>
       </select>
       <input id="quantity" value="${quantity}" onChange="${handleInputQuantity}" placeholder="수량"/>
-      <select id="unit" value="${ingredient}" onChange="${handleUnitSelect}">
+      <select id="unit" value="${unit}" onChange="${handleUnitSelect}">
         <option value="개">개</option>
         <option value="조각">조각</option>
         <option value="그램">그램</option>
@@ -32,17 +32,26 @@ function AddItem() {
     `;
     home.append(newItem);
   };
-  const calculate = () =>{
+  const calculate = () => {
     console.log("계산");
   };
+  useEffect(() => {
+    fetch("/data/foods.csv")
+      .then((response) => response.text())
+      .then((data) => {
+        data.split("\n").forEach((row) => {
+          const fields = row.split(",");
+          console.log(`row: ${row}`);
+        });
+      });
+  }, []);
   return (
     <div id="home" className="home-container">
       <div className="home-item">
         <button onClick={addItem}>추가</button>
         <button onClick={calculate}>계산</button>
       </div>
-      <div className="home-item">
-      </div>
+      <div className="home-item"></div>
     </div>
   );
 }
