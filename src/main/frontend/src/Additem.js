606145c973e26bbed5f712081d7fb41fb9cf3aaa
aaronktaey <aaronktaey@gmail.com>
2023-04-09 16:55:04 +0900

    js 함수 분리

--- src/main/frontend/src/Additem.js
+++ src/main/frontend/src/Additem.js
@@ -1,32 +1,11 @@
 import { useState, useEffect } from "react";
+import { calculate, addItem }from "./Additem_function"
 
 function AddItem() {
   const [ingredient, setIngredient] = useState("");
   const [quantity, setQuantity] = useState("");
   const [unit, setUnit] = useState("");
 
-  const addItem = (e) => {
-    const home = document.querySelector("#home");
-    const newItem = document.createElement("div");
-    newItem.setAttribute("class", "home-item");
-    newItem.innerHTML = `
-      <select id="ingredient" value="${ingredient}" onChange="${handleIngredientSelect(setIngredient)}">
-        <option value="banana">바나나</option>
-        <option value="apple">사과</option>
-        <option value="orange">오렌지</option>
-      </select>
-      <input id="quantity" value="${quantity}" onChange="${handleInputQuantity(setQuantity)}" placeholder="수량"/>
-      <select id="unit" value="${unit}" onChange="${handleUnitSelect(setUnit)}">
-        <option value="개">개</option>
-        <option value="조각">조각</option>
-        <option value="그램">그램</option>
-      </select>
-    `;
-    home.append(newItem);
-  };
-  const calculate = () => {
-    console.log("계산");
-  };
   useEffect(() => {
     fetch("/data/foods.csv")
       .then((response) => response.text())
@@ -40,7 +19,7 @@ function AddItem() {
   return (
     <div id="home" className="home-container">
       <div className="home-item">
-        <button onClick={addItem}>추가</button>
+        <button onClick={addItem(ingredient,quantity, unit ,setIngredient, setQuantity, setUnit)}>추가</button>
         <button onClick={calculate}>계산</button>
       </div>
       <div className="home-item"></div>
--- src/main/frontend/src/Additem_function.js
+++ src/main/frontend/src/Additem_function.js
@@ -1,24 +1,30 @@
-  const handleIngredientSelect = (e) => {
-    setIngredient(e.target.value);
+  function handleIngredientSelect(setIngredient){
+    return(e)=>{
+      setIngredient(e.target.value);
+    }
   };
-  const handleInputQuantity = (e) => {
-    setQuantity(e.target.value);
+  function handleInputQuantity(setQuantity){
+    return(e)=>{
+      setQuantity(e.target.value);
+    }
   };
-  const handleUnitSelect = (e) => {
-    setUnit(e.target.value);
+  function handleUnitSelect(setUnit){
+    return(e)=>{
+      setUnit(e.target.value);
+    }
   };
-  const addItem = (e) => {
+  export function addItem(ingredient, quantity, unit, setIngredient, setQuantity, setUnit){
     const home = document.querySelector("#home");
     const newItem = document.createElement("div");
     newItem.setAttribute("class", "home-item");
     newItem.innerHTML = `
-      <select id="ingredient" value="${ingredient}" onChange="${handleIngredientSelect}">
+      <select id="ingredient" value="${ingredient}" onChange="${handleIngredientSelect(setIngredient)}">
         <option value="banana">바나나</option>
         <option value="apple">사과</option>
         <option value="orange">오렌지</option>
       </select>
-      <input id="quantity" value="${quantity}" onChange="${handleInputQuantity}" placeholder="수량"/>
-      <select id="unit" value="${unit}" onChange="${handleUnitSelect}">
+      <input id="quantity" value="${quantity}" onChange="${handleInputQuantity(setQuantity)}" placeholder="수량"/>
+      <select id="unit" value="${unit}" onChange="${handleUnitSelect(setUnit)}">
         <option value="개">개</option>
         <option value="조각">조각</option>
         <option value="그램">그램</option>
@@ -26,16 +32,8 @@
     `;
     home.append(newItem);
   };
-  const calculate = () => {
-    console.log("계산");
+  export function calculate(){
+    return()=>{
+      console.log("계산");
+    }
   };
-  useEffect(() => {
-    fetch("/data/foods.csv")
-      .then((response) => response.text())
-      .then((data) => {
-        data.split("\n").forEach((row) => {
-          // const fields = row.split(",");
-          console.log(`row: ${row}`);
-        });
-      });
-  }, []);
